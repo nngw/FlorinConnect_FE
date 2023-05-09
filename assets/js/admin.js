dfdb89608d55ef4b63f5268774ce937b256b2f46
Tom Burzynski <tellur.beryl@gmail.com>
2023-05-09 18:15:40 +0100

    add some functions to admin.js

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -4,7 +4,7 @@ const displayAllUsers = document.getElementById('displayAllUsers');
 
 const createPosts = (data) => {
 	displayResults.innerHTML = '';
-	data.forEach((post) => {
+	data.forEach(async (post) => {
 		const card = document.createElement('div');
 		card.classList.add('card', 'flex-row', 'mb-4');
 		card.setAttribute('style', 'min-height: 15rem; height: 100%; width: 635px;');
@@ -22,13 +22,6 @@ const createPosts = (data) => {
 		cardBody.setAttribute('style', 'width: 50%; min-height: 100%');
 		card.appendChild(cardBody);
 
-		if (post.accepted) {
-			cardBody.setAttribute('style', 'border: solid 2px green');
-		}
-		if (post.completed) {
-			cardBody.setAttribute('style', 'border: solid 2px red');
-		}
-
 		const cardHeading = document.createElement('h5');
 		cardHeading.className = 'card-title';
 		cardHeading.textContent = post.title;
@@ -39,6 +32,37 @@ const createPosts = (data) => {
 		cardParagraph.textContent = post.content;
 		cardBody.appendChild(cardParagraph);
 
+		const cardCreatedBy = document.createElement('p');
+		cardCreatedBy.className = 'card-text';
+		cardCreatedBy.textContent = 'Added by: ' + post.added_by;
+		if (post.added_by === undefined) {
+			cardCreatedBy.textContent = 'Added by: admin';
+		}
+
+		cardBody.appendChild(cardCreatedBy);
+
+		if (post.accepted) {
+			cardBody.setAttribute('style', 'border: solid 2px green');
+		}
+		if (post.completed) {
+			cardBody.setAttribute('style', 'border: solid 2px red');
+		}
+
+		const cardAcceptedBy = document.createElement('p');
+		cardAcceptedBy.className = 'card-text';
+		cardAcceptedBy.textContent = 'Accepted By: ' + (await getUserByUsername(post.accepted_by_id));
+
+		if ((await getUserByUsername(post.accepted_by_id)) === undefined) {
+			cardAcceptedBy.textContent = 'Accepted By: -';
+		}
+
+		cardBody.appendChild(cardAcceptedBy);
+
+		const cardDateCreated = document.createElement('p');
+		cardDateCreated.className = 'card-text';
+		cardDateCreated.textContent = 'Date Created: ' + post.date_created;
+		cardBody.appendChild(cardDateCreated);
+
 		const cardButton = document.createElement('a');
 		cardButton.classList.add('btn', 'btn-primary');
 		cardButton.setAttribute('href', '../../index.html');
@@ -47,6 +71,16 @@ const createPosts = (data) => {
 
 		displayResults.appendChild(card);
 	});
+};
+
+const getUserByUsername = async (id) => {
+	try {
+		const res = await fetch(`https://florinconnectapi.onrender.com/users/${id}`);
+		const user = await res.json();
+		return user.username;
+	} catch (error) {
+		console.error(error);
+	}
 };
 
 displayAllPosts.addEventListener('click', async (e) => {
