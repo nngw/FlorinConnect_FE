29d858c64fe0049575f35d6aa0e1094b61773c9e
nngw <thanushiyan@hotmail.co.uk>
2023-05-10 12:24:48 +0100

    modify post cards

--- assets/js/search.js
+++ assets/js/search.js
@@ -19,22 +19,15 @@ const createPosts = (data) => {
 		img.className = 'card-img-top';
 		const image_url = post.image_url || 'https://images.unsplash.com/photo-1609087998060-f567d481a1ae?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80';
 		img.setAttribute('src', `${image_url}`);
-		img.setAttribute('style', 'object-fit: cover; width: 20%; height: 200px; display: flex; align-items: center; justify-items: center;');
+		img.setAttribute('style', 'object-fit: cover; width: 15%; height: 200px; display: flex; align-items: center; justify-items: center;');
 		img.setAttribute('alt', 'Post image');
 		card.appendChild(img);
 
 		const cardBody = document.createElement('div');
 		cardBody.className = 'card-body';
-		cardBody.setAttribute('style', 'width: 10%; min-height: 100%; background-color: #404040; color: #fff;');
+		cardBody.setAttribute('style', 'width: 100%; min-height: 100%; background-color: #404040; color: #fff;');
 		card.appendChild(cardBody);
-
-		// if (post.accepted) {
-		// 	cardBody.setAttribute('style', 'background: green');
-		// }
-		// if (post.completed) {
-		// 	cardBody.setAttribute('style', 'background: red');
-		// }
-
+		
 		const cardHeading = document.createElement('h5');
 		cardHeading.className = 'card-title';
 		cardHeading.setAttribute('style', 'font-weight:600;')
@@ -46,12 +39,39 @@ const createPosts = (data) => {
 		cardParagraph.textContent = post.content;
 		cardBody.appendChild(cardParagraph);
 
-		const cardButton = document.createElement('a');
-		cardButton.classList.add('btn', 'btn-primary');
-		cardButton.setAttribute('href', '../../index.html');
-		cardButton.innerHTML = 'Accept';
-		cardBody.appendChild(cardButton);
+		const cardFooter = document.createElement('div');
+		cardFooter.className = 'post-task-button';
+		cardFooter.setAttribute('style', 'width: 20%; min-height: 100%; background-color: #404040; color: #fff; border-radius:0px;');
+		card.appendChild(cardFooter);
 
+		const cardStatusDisplay = document.createElement('div');
+		cardStatusDisplay.className = 'card-footer';		
+
+		const cardButton = document.createElement('a');
+		console.log(post.accepted)
+
+		if (post.completed === true){
+			cardButton.classList.add('btn', 'carousel-button', 'status-task-buttons');
+			cardButton.innerHTML = 'Task<br>Completed';
+			cardFooter.appendChild(cardButton);	
+			cardStatusDisplay.setAttribute('style', 'border-radius:0px;background-color:#94FBAB;');
+		} else if (post.accepted === true) {
+			cardButton.classList.add('btn', 'carousel-button', 'status-task-buttons');
+			cardButton.setAttribute('href', '../../index.html');
+			cardButton.innerHTML = 'Mark as<br>completed';
+			cardFooter.appendChild(cardButton);
+			cardStatusDisplay.setAttribute('style', 'border-radius:0px;background-color:#7D84B2;');
+		} else if (post.open === true) {
+			cardButton.classList.add('btn', 'carousel-button', 'status-task-buttons');
+			cardButton.setAttribute('href', '../../index.html');
+			cardButton.innerHTML = 'Accept<br>task';
+			cardFooter.appendChild(cardButton);
+			cardStatusDisplay.setAttribute('style', 'border-radius:0px;background-color:#ED254E;');
+		}
+		// cardButton.setAttribute('style', 'display: ;');
+		card.appendChild(cardStatusDisplay);
+		
+		
 		results.appendChild(card);
 	});
 };
