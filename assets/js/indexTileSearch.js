a17f71e5801993283d61fffbee1f46f39b5eec48
nngw <thanushiyan@hotmail.co.uk>
2023-05-11 20:30:48 +0100

    add redirect from homepage to filter posts, token functionality still required

--- assets/js/indexTileSearch.js
+++ assets/js/indexTileSearch.js
@@ -1,16 +1,46 @@
-const filterIndexVolunteerTaskClick = document.getElementById('index-recycling-cat-card');
+const filterIndexVolunteerTaskClick = document.getElementById('index-volunteer-cat-card');
 const filterIndexSocialTaskClick = document.getElementById('index-social-cat-card');
 const filterIndexRecyclingTaskClick = document.getElementById('index-recycling-cat-card');
 const filterIndexWorkshopTaskClick = document.getElementById('index-workshop-cat-card');
 
 filterIndexRecyclingTaskClick.addEventListener('click', async (e) => {
 	e.preventDefault();
-	console.log(e)
-	function newLocation() {
-        window.location.href="board.html";
-    }
+	my_url = 'board.html?recycling'
+	
+	document.onclick = await function () {
+		window.location.replace(my_url);	
+	}
 
 })
-filterIndexSocialTaskClick
-filterIndexVolunteerTaskClick
-filterIndexWorkshopTaskClick
+filterIndexSocialTaskClick.addEventListener('click', async (e) => {
+	e.preventDefault();
+	my_url = 'board.html?social'
+	
+	document.onclick = await function () {
+		window.location.replace(my_url);	
+	}
+
+})
+
+filterIndexVolunteerTaskClick.addEventListener('click', async (e) => {
+	e.preventDefault();
+	my_url = 'board.html?volunteer'
+	
+	document.onclick = await function () {
+		window.location.replace(my_url);	
+	}
+
+})
+
+filterIndexWorkshopTaskClick.addEventListener('click', async (e) => {
+	e.preventDefault();
+	my_url = 'board.html?workshop'
+	
+	document.onclick = await function () {
+		window.location.replace(my_url);	
+	}
+
+})
+
+
+
